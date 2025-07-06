729686f1a3d161640a8e232c181cd8a08722308a
stxtiz <basty46@gmail.com>
2025-07-05 21:47:46 -0400

    first commit

---
+++ src/router.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
