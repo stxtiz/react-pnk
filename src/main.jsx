740baeda3a57dbe9dede061fca0b033fc7f750d7
stxtiz <basty46@gmail.com>
2025-07-06 14:49:42 -0400

    Actualizar importación de estilos en main.jsx para usar global.css en lugar de index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "react-bootstrap";
 
-import "./index.css";
+import "./styles/global.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
